51dc60921a503c76c8cf41f2ee1b76c571c930fe
xlwxyswxnnxsleep <79219180+xlwxyswxnnxsleep@users.noreply.github.com>
2021-03-07 19:42:45 +0300

    Update task_2_4.py

--- hm4/task_2_4.py
+++ hm4/task_2_4.py
@@ -1,3 +1,1 @@
-x = lambda age: 'Older' if age > 30 else 'Younger'
-
-print(x(40))
+print(list(filter(lambda check: check % 19 == 0 or check % 30 == 0, [19, 65, 57, 39, 152, 639, 121, 44, 90, 190])))
